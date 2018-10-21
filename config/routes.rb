78790542c15b898153a4ca28c1b826e7d665e0e2
Victor Tovar <tovarvictor986@gmail.com>
2018-10-21 14:59:53 -0500

    Capitulo 5 creation form, migration...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "welcome#index"
+end
+
+Ejercicio1::Application.routes.draw do
+  resources :articles
+  root 'welcome#index'
 end
