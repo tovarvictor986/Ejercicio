8159fcd000f3f9f0e760700e6983d5104f04f713
Victor Tovar <tovarvictor986@gmail.com>
2018-10-21 15:29:45 -0500

    Capitulo 5 validation...

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,11 @@ class ArticlesController < ApplicationController
 	
 	def create
 		@article = Article.new(params[:article]) 
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
 	
 	private
@@ -15,5 +18,9 @@ class ArticlesController < ApplicationController
 	  
 	def show
 	  @article = Article.find(params[:id])
+	end
+	
+	def index
+	  @articles = Article.all
 	end
 end
